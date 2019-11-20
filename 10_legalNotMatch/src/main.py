d8bb608030b02781156454e1cb63aa892315ee50
Vic <fengweiqi@sjtu.edu.cn>
2019-11-20 21:55:02 +0800

    完成适用法律条文不当

--- 10_legalNotMatch/src/main.py
+++ 10_legalNotMatch/src/main.py
@@ -19,7 +19,12 @@ from keras.utils import np_utils
 from keras.models import load_model
 from keras import regularizers
 from keras.callbacks import EarlyStopping
+import re
+# Import for testing baseline
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
+INPUT = 30
 
 def loadData():
 	"""
@@ -30,37 +35,50 @@ def loadData():
 	
 	返回:
 		papers: 		含有案件信息的list
-		paper_labels:	含有案件标签的list
 		word_embedding: 预先训练的 word2vec 模型
 	"""
 	papers = []
+	# 第一个参数作为被读入的json文件路径
+	filename = sys.argv[1]
+	file = open(filename,'r')
+	# 将读取的数据存入 papers list 中
+	for line in file.readlines():
+		dic = json.loads(line)
 
-	paper_labels = []
-	filenames = []
-	filenames.append("../data/criminal.json")
-	filenames.append("../data/civil.json")
-	filenames.append("../data/admin.json")
-	labels = [0, 1, 2]
-	for filename, label in zip(filenames, labels):
-		# 将读取的案件数据存入 papers list 中
-		with open(filename, 'r') as file:
-			for line in file.readlines():
-				dic = json.loads(line)
-				papers.append(dic)
-				paper_labels.append(label)
+	for key in dic.keys():
+		papers.append(dic[key])
 	# 导入预训练的 word2vec model
 	word_embedding = word2vec.load('../data/word2vec_result.bin')
 	print("---已经成功导入数据 {}---".format(filename))
-	return papers, paper_labels, word_embedding
 
+	return papers, word_embedding
 
-def preprocessData(papers, paper_labels, word_embedding):
+def extractLaw(law):
+	# 只保留第一条法律
+	begin = -1
+	end = -1
+	for i in range(len(law)):
+		if begin == -1 and law[i] == "《":
+			begin = i
+			break
+	for i in range(begin, len(law)):
+		if law[i] == "，" or law[i] == "、":
+			end = i
+			break
+	if end != -1:
+		law = law[begin: end]
+	else:
+		law = law[begin:]
+
+	law = re.findall(r"《.*》第.*条", law)
+	return law
+
+def preprocessData(papers, word_embedding):
 	"""
 	对案件关键词编码, 并生成训练集和测试集
 
 	参数:
 		papers: 		含有案件信息的list
-		paper_labels:	含有案件标签的list
 		word_embedding: 预先训练好的word2vec 模型
 	
 	返回:
@@ -78,15 +96,28 @@ def preprocessData(papers, paper_labels, word_embedding):
 	test_target = []
 	train_data_ratio = 0.8
 	# 每一个案件抽取的关键词数量
-	word_nums = 100
+	word_nums = INPUT
 	# "认为" 中提取的关键词数量
-	word_num_1 = 96
-	# "reason" 中提取的关键词数量
-	word_num_3 = 1
-	# "docName" 中提取的关键词数量
-	word_num_2 = word_nums - word_num_1 - word_num_3
+	word_num_1 = 20
+	# "全文" 中提取的关键词数量
+	word_num_2 = word_nums - word_num_1
+	# 创建 "法律条文" 字典 ("法律条文": "法律条文 ID")
+
+	laws = [paper["法律条文名称"][0] for paper in papers if '法律条文名称' in paper.keys() and paper['法律条文名称'] != []]
+	allLaws = []
+	# 提取法律条文信息
+	for law in laws:
+		law = extractLaw(law)
+		if law != []:
+			allLaws.append(law[0])
+	laws = list(set(allLaws))
+	law_pairs = [(law, law_id) for (law_id, law) in enumerate(laws)]
+	law_dict = dict(law_pairs)
+
 	# 依次对每一个案件进行处理
 	for (paper_id, paper) in enumerate(papers):
+		if '本院认为' not in paper.keys() or paper['本院认为'] == [] or '法律条文名称' not in paper.keys() or paper['法律条文名称'] == []:
+			continue
 		# 提取含有认为信息的所有 keys
 		keys = [item for item in paper if "认为" in item]
 		# 判断当前案件是否有效
@@ -94,32 +125,36 @@ def preprocessData(papers, paper_labels, word_embedding):
 			continue
 		# 提取有效案件信息
 		key = keys[0]
-		length = len(HanLP.segment(paper[key]))
+		length = len(HanLP.segment(paper[key][0]))
 		# 从 "认为" 对应的信息中提取 
-		words = list(HanLP.extractKeyword(paper[key], min(word_num_1, length)))
+		words = list(HanLP.extractKeyword(paper[key][0], min(word_num_1, length)))
 		# 从 "docname" 对应的信息进行提取
-		words += list(HanLP.extractKeyword(paper['docName'], word_num_2))
-		# 从 "reason" 对应的信息进行提取
-		words += list(HanLP.extractKeyword(paper['reason'], word_num_3))
+		length = len(HanLP.segment(paper['全文'][0]))
+		words += list(HanLP.extractKeyword(paper['全文'][0], min(word_num_2, length)))
 		# 去除无用的字符
 		if_valid = lambda word: word != '\n' and word != ' '
 		words = [word for word in words if if_valid(word)]
 		# 得到关键词编码的词向量
-		vecs = [word_embedding[word].reshape(1, -1) for word in words]
+		vecs = [word_embedding[word].reshape(1, -1) for word in words if word in word_embedding.vocab]
+		print(len(vecs))
 		# 将其处理成矩阵
 		word_matrix = np.concatenate(vecs, 0)	
-		# 填充均值使其为 (1, 100, 100)的形状
+		# 填充均值使其为 (1, 30, 100)的形状
 		pad_width = ((0, word_nums - word_matrix.shape[0]), (0, 0))
 		word_matrix = np.pad(word_matrix, pad_width, 'mean')
 		word_matrix = np.expand_dims(word_matrix, 0)
+		# 提取原因
+		law = extractLaw(paper['法律条文名称'][0])
+		if law == []:
+			continue
 		# 根据随机数分到 训练集 或者是 测试集中
 		if np.random.rand() < train_data_ratio:
 			train_data.append(word_matrix)
-			train_target.append(paper_labels[paper_id])
+			train_target.append(law_dict[law[0]])
 		else:
 			test_data.append(word_matrix)
-			test_target.append(paper_labels[paper_id])
-
+			test_target.append(law_dict[law[0]])
+		print("处理进度: {} / {}".format(paper_id, len(papers)))
 	# 整理成 numpy 3D array 格式
 	train_data = np.concatenate(train_data)
 	test_data = np.concatenate(test_data)
@@ -127,10 +162,15 @@ def preprocessData(papers, paper_labels, word_embedding):
 	test_target = np.array(test_target)
 	# 存入文件 加速开发流程
 	print("---已经成功产生训练集和测试集---")
-	return train_data, train_target, test_data, test_target
+	np.save("../data/dic/dic_train_data.npy", train_data)
+	np.save("../data/dic/dic_train_target.npy", train_target)
+	np.save("../data/dic/dic_test_data.npy", test_data)
+	np.save("../data/dic/dic_test_target.npy", test_target)
+	np.save("../data/dic/dic_law_dict.npy", law_dict)
+	return train_data, train_target, test_data, test_target, law_dict
 
 
-def trainModel(train_data, train_target, reason_num):
+def trainModel(train_data, train_target, law_num):
 	"""
 	训练神经网络模型, 并保存模型训练文件
 
@@ -144,7 +184,7 @@ def trainModel(train_data, train_target, reason_num):
 	"""
 	# 定义神经网络模型
 	model = Sequential()
-	model.add(GRU(384,input_shape=(100,100)))
+	model.add(GRU(384,input_shape=(INPUT,100)))
 	model.add(Dropout(0.5))
 	model.add(Dense(units=384,activation = 'relu'))
 	model.add(Dropout(0.5))
@@ -154,19 +194,50 @@ def trainModel(train_data, train_target, reason_num):
 	model.add(Dropout(0.5))
 	model.add(Dense(units=384,activation = 'relu'))
 	model.add(Dropout(0.5))
-	model.add(Dense(units=reason_num, activation = 'softmax'))
+	model.add(Dense(units=law_num, activation = 'softmax'))
 
 	# 编译神经网络模型
-	model.compile(loss='categorical_crossentropy',optimizer = 'adam',metrics=['accuracy'])
+	model.compile(loss='categorical_crossentropy',optimizer = 'adam',metrics=['acc'])
 	# 防止过拟合
-	early_stopping = EarlyStopping(monitor='val_loss', patience=50,verbose=1, mode='auto')
-	train_target = np_utils.to_categorical(train_target, num_classes=reason_num)
+	train_target = np_utils.to_categorical(train_target, num_classes=law_num)
 	# 开始训练模型
-	history = model.fit(train_data, train_target, validation_split=0.15, batch_size=500, epochs=500, callbacks=[early_stopping])
-	model.save('../model/model.h5')
+	history = model.fit(train_data, train_target, validation_split=0.15, batch_size=50, epochs=1500)
+	saveFig(history, "../visual/dic_loss.png", "../visual/dic.png")
+	model.save('../model/dic.h5')
 	print("---训练模型完毕---")
 
-def testModel(train_data, train_target, test_data, test_target, reason_num):
+
+def saveFig(history, loss_file, acc_file):
+	"""
+	可视化并保存图片
+
+	参数:
+		history: 	keras model.fit() 的返回值
+		loss_file:	loss 可视化图片名
+		acc_file:	acc 可视化图片名
+
+	返回:	
+		None
+	"""
+	# Plot training & validation accuracy values
+	plt.plot(history.history['acc'])
+	plt.plot(history.history['val_acc'])
+	plt.title('Model accuracy')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Test'], loc='upper left')
+	plt.savefig(loss_file)
+	plt.clf()
+	# Plot training & validation loss values
+	plt.plot(history.history['loss'])
+	plt.plot(history.history['val_loss'])
+	plt.title('Model loss')
+	plt.ylabel('Loss')
+	plt.xlabel('Epoch')
+	plt.legend(['Train', 'Test'], loc='upper left')
+	plt.savefig(acc_file)
+
+def testModel(train_data, train_target, test_data, test_target, law_num):
 	"""
 	将训练好的模型在训练集和测试集进行测试, 并打印相关信息
 
@@ -180,11 +251,11 @@ def testModel(train_data, train_target, test_data, test_target, reason_num):
 		None
 	"""
 	# 加载预训练模型
-	model = load_model("../model/model.h5")
+	model = load_model("../model/dic.h5")
 	# 转化成 one-hot 矩阵
-	train_target = np_utils.to_categorical(train_target, num_classes=reason_num)
+	train_target = np_utils.to_categorical(train_target, num_classes=law_num)
 	# 转化成 one-hot 矩阵
-	test_target = np_utils.to_categorical(test_target, num_classes=reason_num)
+	test_target = np_utils.to_categorical(test_target, num_classes=law_num)
 	result = model.evaluate(train_data, train_target)
 	print("训练集准确率: {:.3f}".format(result[1]))
 	result = model.evaluate(test_data, test_target)
@@ -193,20 +264,73 @@ def testModel(train_data, train_target, test_data, test_target, reason_num):
 
 def legalNotMatch():
 	"""
-	判断适用法律不当的主函数
+	判断案由不当的主函数
 	"""
 	# 不显示无用信息
 	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 	# 从输入的 json 文件提取案件信息, 并且返回预训练的词向量模型
-	papers, paper_labels, word_embedding = loadData()
+	papers, word_embedding = loadData()
 	# 从案件信息中提取训练集和测试集的
-	train_data, train_target, test_data, test_target = preprocessData(papers, paper_labels, word_embedding)
+	# train_data, train_target, test_data, test_target, law_dict = preprocessData(papers, word_embedding)
+
+	train_data, train_target, test_data, test_target, law_dict = loadDicData()
 	# 训练我们的模型
-	# trainModel(train_data, train_target, 3)
+	trainModel(train_data, train_target, len(law_dict))
 	# 测试我们的模型
-	testModel(train_data, train_target, test_data, test_target, 3)
-	print("---适用法律不当检测完毕---")
+	testModel(train_data, train_target, test_data, test_target, len(law_dict))
+
+	print("---案由不当检测完毕---")
+
+def loadDicData():
+	"""
+	导入实现处理好的 Dic 数据
+	"""
+	train_data = np.load("../data/dic/dic_train_data.npy")
+	train_target = np.load("../data/dic/dic_train_target.npy")
+	test_data = np.load("../data/dic/dic_test_data.npy")
+	test_target = np.load("../data/dic/dic_test_target.npy")
+	reason_dict = np.load("../data/dic/dic_law_dict.npy", allow_pickle=True).item()
+
+	return train_data, train_target, test_data, test_target, reason_dict
+
+
+def baselineSVM():
+	"""
+	用 SVM 作为 baseline 模型
+	"""
+	# 导入 Criminal 的数据
+	train_data, train_target, test_data, test_target, law_dict = loadDicData()
 	
+	# 重新 Reshape 训练数据和测试数据
+	train_data = train_data.reshape(train_data.shape[0], -1)
+	test_data = test_data.reshape(test_data.shape[0], -1)
+	train_target = train_target.reshape(-1, 1)
+	test_target = test_target.reshape(-1, 1)
+	# 用 SVM 作为baseline
+	clf = SVC(gamma=10.0, verbose=True)
+	clf.fit(train_data, train_target)
+	print(clf.score(train_data, train_target))
+	print(clf.score(test_data, test_target))
+
+
+def baselineRandomForest():
+	"""
+	用 Random Forest 作为 baseline
+	"""
+	# 导入 Criminal 的数据
+	train_data, train_target, test_data, test_target, law_dict = loadDicData()
+	train_data = train_data.reshape(train_data.shape[0], -1)
+	test_data = test_data.reshape(test_data.shape[0], -1)
+	train_target = train_target.reshape(-1, 1)
+	test_target = test_target.reshape(-1, 1)
+	# 用 SVM 作为baseline
+	clf = RandomForestClassifier(n_estimators=100, max_depth=20, random_state=0)
+	clf.fit(train_data, train_target)
+	print(clf.score(train_data, train_target))
+	print(clf.score(test_data, test_target))
+
+
 if __name__=='__main__':
-	legalNotMatch()
+	baselineRandomForest()
+
 
